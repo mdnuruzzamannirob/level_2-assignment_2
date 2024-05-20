fd7b9190f62f07f3af956a529acf795e674369cd
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-05-21 01:59:40 +0600

    initialized the project

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, process: "readonly" },
+    },
+
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+    ignores: ["**/node_modules/", ".dist/"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
