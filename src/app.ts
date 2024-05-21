adab78ddd8256bcb958b530acbf4e7b7e619c5d9
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-05-22 01:36:47 +0600

    added global error handler and route not found error handler function

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { ProductRoutes } from './app/modules/product/product.route';
 import { OrderRoutes } from './app/modules/order/order.route';
@@ -15,6 +15,23 @@ app.use('/api/orders', OrderRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Dear! this is level-2 assignment-2');
+});
+
+// Middleware to handle 404 Not Found errors
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
+
+// Global error-handling middleware
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  // Handle other errors
+  res.status(error.status || 500).json({
+    success: false,
+    message: error.message || 'Internal Server Error',
+  });
 });
 
 export default app;
